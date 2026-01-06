cbebb5f77d601135be5fbb646b83d6c5db5f0a30
jushyi <maserinj@gmail.com>
2026-01-06 11:53:46 -0500

    index file to export firebase services

---
+++ src/services/firebase/index.js
@@ -0,0 +1,39 @@
+// Export Firebase configuration and instances
+export { app, auth, db, storage } from './firebaseConfig';
+
+// Export Auth service functions
+export {
+  signUpWithEmail,
+  signInWithEmail,
+  signOutUser,
+  resetPassword,
+  updateUserProfile,
+  getCurrentUser,
+} from './authService';
+
+// Export Firestore service functions
+export {
+  createUserDocument,
+  getUserDocument,
+  updateUserDocument,
+  createPhotoDocument,
+  getUserPhotos,
+  updatePhotoDocument,
+  deletePhotoDocument,
+  createFriendship,
+  acceptFriendship,
+  getUserFriends,
+  createNotification,
+  markNotificationAsRead,
+  createPhotoView,
+  hasUserViewedPhoto,
+} from './firestoreService';
+
+// Export Storage service functions
+export {
+  uploadProfilePhoto,
+  uploadPhoto,
+  deleteProfilePhoto,
+  deletePhoto,
+  getPhotoURL,
+} from './storageService';
