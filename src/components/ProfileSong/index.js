23b87820d9871f13267ecb142543fcd70659a47e
jushyi <maserinj@gmail.com>
2026-01-28 16:23:44 -0500

    chore(07.3-01): delete WaveformScrubber component
    - Delete src/components/ProfileSong/WaveformScrubber.js
    - Remove WaveformScrubber export from index.js
    - No longer needed since ClipSelectionModal uses full 30s preview

--- src/components/ProfileSong/index.js
+++ src/components/ProfileSong/index.js
@@ -1,4 +1,3 @@
 export { default as ProfileSongCard } from './ProfileSongCard';
 export { default as SongSearchResult } from './SongSearchResult';
-export { default as WaveformScrubber } from './WaveformScrubber';
 export { default as ClipSelectionModal } from './ClipSelectionModal';
